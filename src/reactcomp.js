767ed39e42f5bd31a9b81716df3c552d736dbc3d
charity <wa.nelimacharity@gmail.com>
2023-09-09 14:21:10 +0300

    react components eg

---
+++ src/reactcomp.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class MyComponent extends React.Component {
+  // The render method defines what the component should render.
+  render() {
+    return (
+      <div>
+        <h1>Hello, React!</h1>
+        <p>This is a simple React component.</p>
+      </div>
+    );
+  }
+}
+
+export default MyComponent;
