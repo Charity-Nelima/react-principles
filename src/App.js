f135de866187c4cdfab41c0ddb7457ba2165073c
charity <wa.nelimacharity@gmail.com>
2023-09-09 14:14:46 +0300

    react components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+// Components:
+
+// In React, everything is built using components. Components are reusable,
+//  self-contained building blocks for your UI. Each component can have its own state and props (properties).
 
 export default App;
