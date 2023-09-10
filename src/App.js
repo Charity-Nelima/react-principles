0f3717d04d67d00e77994d200e51324819a80ffd
charity <wa.nelimacharity@gmail.com>
2023-09-10 14:15:44 +0300

    components folder

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import React from 'react';
+import reactcomp from './reactcomp';
+
+function App(){
+
+    return(
+        <div>
+        <reactcomp  />
+        </div>
+    )
+}
 
 
+// // Components:
 
+// // In React, everything is built using components. Components are reusable,
+// //  self-contained building blocks for your UI. Each component can have its own state and props (properties).
 
-
-// Components:
-
-// In React, everything is built using components. Components are reusable,
-//  self-contained building blocks for your UI. Each component can have its own state and props (properties).
 
 export default App;
